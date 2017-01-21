50fa04a4282106a2307306a39bf38be245a24da5
Raj Sharma <raj.sharma@orasi.com>
2017-01-21 18:04:54 -0500

    Add files via upload

---
+++ seleniumTests/TrainingExerciseSuite2.java
@@ -0,0 +1,13 @@
+package exercise2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ iFrame.class, 
+				sAlerts.class, 
+				TextBox.class })
+public class TrainingExerciseSuite2 {
+
+}
---
+++ seleniumTests/TrainingExercisesSuite.java
@@ -0,0 +1,16 @@
+package exercise2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ basicAuth.class, 
+				ClickCheckbox.class, 
+				dropdown.class, 
+				DynamicControls.class})
+public class TrainingExercisesSuite {
+	
+
+}
